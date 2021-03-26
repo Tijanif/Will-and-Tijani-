1cd41886fee2f173b46d6204940c32eeebbd9895
Will Gibson <will.gibson.contractor@dvla.gov.uk>
2021-03-26 17:21:46 +0000

    Handing back to Tijani

--- src/katas/mumble/mumbleLetters.js
+++ src/katas/mumble/mumbleLetters.js
@@ -1,5 +1,10 @@
-function mumbleLetters () {
-  return "A";
+function mumbleLetters (input) {
+  const outputArray = [];
+  const letters = input.split('');
+  letters.forEach(letter => {
+    outputArray.push(letter.toUpperCase());
+  });
+  return outputArray.join('-');
 }
 
 module.exports = mumbleLetters;
