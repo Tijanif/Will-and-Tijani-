733f2dd8984c8096e1bfb682a0bfb889cbba4f99
Tijani Fulani <Fulani07@gmail.com>
2021-03-26 17:11:29 +0000

    created new failing test

--- src/katas/mumble/mumbleLetters.test.js
+++ src/katas/mumble/mumbleLetters.test.js
@@ -5,4 +5,9 @@ describe("mumbleLetters", () => {
     test('with "a" returns "A"', () => {
         expect(mumbleLetters('a')).toBe('A');
     });
+    
+    test('with "abC" returns "A-Bb-Ccc"', () => {
+        expect(mumbleLetters('abC')).toBe('A-Bb-Ccc');
+    });
 });
+
